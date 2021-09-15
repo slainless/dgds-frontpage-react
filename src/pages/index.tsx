e518612551b63a910c7590fb66019e695df33154
Ahmad Aiman <aiman.fauzy.55@gmail.com>
2021-09-15 14:39:29 +0800

    fix index to use page query

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,66 +13,66 @@ import TestimonySection from "components/pages/index/testimony"
 import UsedBySection from "components/pages/index/user"
 import SponsorSection from "components/pages/index/sponsor"
 
-export default function IndexPage() {
-  const data = useStaticQuery(graphql`
-    query {
-      comparison: allComparisonYaml {
-        nodes {
-          iconSrc: icon
-          title
-          plus
-          min
-        }
+export const query = graphql`
+  query {
+    comparison: allComparisonYaml {
+      nodes {
+        iconSrc: icon
+        title
+        plus
+        min
       }
+    }
 
-      solution: allSolutionYaml {
-        nodes {
-          iconSrc: icon
-          title
-          content
-          media {
-            src
-            width
-          }
-        }
-      }
-
-      statistic: allStatisticYaml {
-        nodes {
-          iconSrc: icon
-          name
-          number
-        }
-      }
-
-      testimony: allTestimonyYaml {
-        nodes {
-          media {
-            src
-          }
-          name
-          title
-          content
-        }
-      }
-
-      user: allUserYaml {
-        nodes {
-          iconSrc: icon
-          name
-          number
-        }
-      }
-
-      sponsor: allSponsorYaml {
-        nodes {
-          name
-          iconSrc: icon
+    solution: allSolutionYaml {
+      nodes {
+        iconSrc: icon
+        title
+        content
+        media {
+          src
+          width
         }
       }
     }
-  `)
 
+    statistic: allStatisticYaml {
+      nodes {
+        iconSrc: icon
+        name
+        number
+      }
+    }
+
+    testimony: allTestimonyYaml {
+      nodes {
+        media {
+          src
+        }
+        name
+        title
+        content
+      }
+    }
+
+    user: allUserYaml {
+      nodes {
+        iconSrc: icon
+        name
+        number
+      }
+    }
+
+    sponsor: allSponsorYaml {
+      nodes {
+        name
+        iconSrc: icon
+      }
+    }
+  }
+`
+
+export default function IndexPage({ data }) {
   return (
     <MainLayout>
       <TitleSection/>
